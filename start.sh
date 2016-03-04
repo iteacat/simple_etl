ba5501deab0854ac14de5670e70c064eeea0c94e
iteacat <justwy@gmail.com>
2016-03-03 22:14:22 -0500

    script

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ platform=`uname -s`
 ./bin/$platform/setup-mongo.sh
 ./bin/$platform/start-mongo.sh
 npm install
-node --stack-size=2000000 ./main/index
+NODE_ENV=prod node --stack-size=2000000 ./main/index
